4a4e7eff128a31a13da4ba1145e98895c2c8b246
Yunus Doğan <yunusdgntr@gmail.com>
2022-04-21 05:03:07 +0300

    first commit

---
+++ Scrap.Cars.Service/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Scrap.Cars.Service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
